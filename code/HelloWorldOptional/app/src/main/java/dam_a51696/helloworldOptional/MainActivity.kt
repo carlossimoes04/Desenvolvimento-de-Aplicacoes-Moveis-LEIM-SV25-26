0d200f78785b8518b14c87139d9bc0fa08dedd0b
Carlos Simões <carlossimoes2004@live.com.pt>
2026-02-28 22:53:14 +0000

    Finished section 5.3, where was created an app that displays information about the current device build

--- code/HelloWorldOptional/app/src/main/java/dam_a51696/helloworldOptional/MainActivity.kt
+++ code/HelloWorldOptional/app/src/main/java/dam_a51696/helloworldOptional/MainActivity.kt
@@ -1,20 +1,39 @@
 package dam_a51696.helloworldOptional
 
+import android.os.Build
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val infoTextView = findViewById<TextView>(R.id.textSystemInfo)
+
+        val info = """
+            Manufacturer: ${Build.MANUFACTURER}
+            Model: ${Build.MODEL}
+            Brand: ${Build.BRAND}
+            Type: ${Build.TYPE}
+            User: ${Build.USER}
+            Incremental: ${Build.VERSION.INCREMENTAL}
+            SDK: ${Build.VERSION.SDK_INT}
+            Version Code: ${Build.VERSION.RELEASE}
+            Display: ${Build.DISPLAY}
+        """.trimIndent()
+
+        infoTextView.text = info
     }
 }
