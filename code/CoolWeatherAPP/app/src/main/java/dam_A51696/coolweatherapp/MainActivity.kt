5e452fdd3db679695a0c29eba888550d6ed54b26
Carlos Simões <carlossimoes2004@live.com.pt>
2026-04-05 11:55:47 +0100

    Completed all the exercises in the "Designing the layout" section, up to point 4.

--- code/CoolWeatherAPP/app/src/main/java/dam_A51696/coolweatherapp/MainActivity.kt
+++ code/CoolWeatherAPP/app/src/main/java/dam_A51696/coolweatherapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package dam_A51696.coolweatherapp
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,9 +9,24 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val day = true
+
+        when (resources.configuration.orientation) {
+            Configuration.ORIENTATION_PORTRAIT -> {
+                if (day) setTheme(R.style.Theme_Day)
+                else setTheme(R.style.Theme_Night)
+            }
+            Configuration.ORIENTATION_LANDSCAPE -> {
+                if (day) setTheme(R.style.Theme_Day_Land)
+                else setTheme(R.style.Theme_Night_Land)
+            }
+        }
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.container)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
