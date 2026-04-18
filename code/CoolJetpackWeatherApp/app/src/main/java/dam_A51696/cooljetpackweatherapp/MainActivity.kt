6a1ed276f59e1798468d3b71ad9f4e82e382a16c
Carlos Simões <carlossimoes2004@live.com.pt>
2026-04-18 19:29:46 +0100

    Updated MainActivity.kt

--- code/CoolJetpackWeatherApp/app/src/main/java/dam_A51696/cooljetpackweatherapp/MainActivity.kt
+++ code/CoolJetpackWeatherApp/app/src/main/java/dam_A51696/cooljetpackweatherapp/MainActivity.kt
@@ -3,45 +3,16 @@ package dam_A51696.cooljetpackweatherapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import dam_A51696.cooljetpackweatherapp.ui.WeatherUI
 import dam_A51696.cooljetpackweatherapp.ui.theme.CoolJetpackWeatherAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             CoolJetpackWeatherAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                WeatherUI()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CoolJetpackWeatherAppTheme {
-        Greeting("Android")
     }
 }
