5b2b08a6dc244b7f4a838a31fe6e5fe68e67edb4
Carlos Simões <carlossimoes2004@live.com.pt>
2026-05-09 14:08:11 +0100

    Completed Task 6

--- intro-coroutines/src/tasks/Request6Progress.kt
+++ intro-coroutines/src/tasks/Request6Progress.kt
@@ -7,5 +7,22 @@ suspend fun loadContributorsProgress(
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: emptyList()
+
+    var usersList = listOf<User>()
+    var isLastRepo = false
+    var counter = 0
+    for (repo in repos) {
+        val users = service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+        usersList = (usersList + users).aggregate()
+        counter ++
+        if (counter == repos.size) isLastRepo = true
+        updateResults(usersList, isLastRepo)
+    }
 }
