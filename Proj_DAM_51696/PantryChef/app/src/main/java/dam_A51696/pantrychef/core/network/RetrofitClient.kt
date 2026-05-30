e31a6d318688bb6e70ed3de26f072c2b80e71dd5
Carlos Simões <carlossimoes2004@live.com.pt>
2026-05-30 22:11:40 +0100

    Deleted an unused file

--- Proj_DAM_51696/PantryChef/app/src/main/java/dam_A51696/pantrychef/core/network/RetrofitClient.kt
+++
@@ -1,9 +0,0 @@
-package dam_A51696.pantrychef.core.network
-
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-
-object RetrofitClient {
-    // Example Retrofit client setup
-    // Hilt should be used to provide Retrofit instances in a real scenario
-}
