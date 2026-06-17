7b423d555760ed1e44cb499677ce86d0b441d53b
Carlos Simões <carlossimoes2004@live.com.pt>
2026-06-17 16:11:17 +0100

    Deleted Result.kt because it isn't being used

--- Proj_DAM_51696/PantryChef/app/src/main/java/dam_A51696/pantrychef/core/error/Result.kt
+++
@@ -1,6 +0,0 @@
-package dam_A51696.pantrychef.core.error
-
-sealed class Result<out T> {
-    data class Success<out T>(val data: T) : Result<T>()
-    data class Error(val exception: Exception) : Result<Nothing>()
-}
