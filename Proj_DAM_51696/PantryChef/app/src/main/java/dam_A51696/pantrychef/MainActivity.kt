b7ddd52045aa78aa69db5749e96e099b4fe038e5
Carlos Simões <carlossimoes2004@live.com.pt>
2026-06-17 14:07:32 +0100

    Added more comments

--- Proj_DAM_51696/PantryChef/app/src/main/java/dam_A51696/pantrychef/MainActivity.kt
+++ Proj_DAM_51696/PantryChef/app/src/main/java/dam_A51696/pantrychef/MainActivity.kt
@@ -8,8 +8,37 @@ import dagger.hilt.android.AndroidEntryPoint
 import dam_A51696.pantrychef.presentation.navigation.AppNavigation
 import dam_A51696.pantrychef.presentation.theme.PantryChefTheme
 
+
+/**
+ * Ponto de entrada principal da UI da aplicação
+ *
+ * Esta classe herda de [ComponentActivity], que é a classe base recomendada
+ * para aplicações que utilizam o Jetpack Compose
+ *
+ * A anotação [AndroidEntryPoint] é crucial: ela indica ao Dagger Hilt que
+ * esta Activity é um ponto de entrada para injeção de dependências. Isto
+ * permite que dependências (como os ViewModels do Hilt) sejam fornecidas
+ * aos ecrãs Compose que são lançados a partir daqui
+ */
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    /**
+     * Chamado quando a Activity está a ser iniciada
+     *
+     * Este método é responsável por:
+     * - Inicializar a Activity base;
+     * - Ativar o modo `EdgeToEdge`, permitindo que o conteúdo da app
+     *    seja desenhado atrás das barras de sistema (barra de estado em cima
+     *    e barra de navegação em baixo), para um design mais imersivo;
+     * - Definir o conteúdo visual da Activity usando o [setContent] do Compose,
+     *    onde é aplicado o tema global [PantryChefTheme] e iniciada a árvore
+     *    de navegação da aplicação com [AppNavigation];
+     *
+     * @param savedInstanceState Se a Activity estiver a ser reiniciada após
+     * ter sido encerrada, este Bundle contém os dados mais recentes fornecidos.
+     * Caso contrário, é null
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
--- Proj_DAM_51696/PantryChef/app/src/main/java/dam_A51696/pantrychef/PantryChefApp.kt
+++ Proj_DAM_51696/PantryChef/app/src/main/java/dam_A51696/pantrychef/PantryChefApp.kt
@@ -3,5 +3,16 @@ package dam_A51696.pantrychef
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+/**
+ * Classe base da aplicação Pantry Chef
+ *
+ * Esta classe herda de [Application] e é o primeiro componente a ser instanciado
+ * quando a aplicação é iniciada, antes de qualquer Activity, Service ou Receiver
+ *
+ * A anotação [HiltAndroidApp] é obrigatória e fundamental, pois desencadeia a
+ * geração de código do Dagger Hilt. Ela cria um contentor de dependências ao
+ * nível da aplicação (Application Component) que serve como base para injetar
+ * dependências (como Repositórios, ViewModels e Bases de Dados) no resto da aplicação
+ */
 @HiltAndroidApp
 class PantryChefApp : Application()
